51dd311e49b119bca6e68b8527323a80534809a1
Victor Carli <victorcarlibr@gmail.com>
2022-04-23 15:38:43 -0300

    feat: logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,13 @@
+import { Image } from '@chakra-ui/react'
+
+export function Logo() {
+  return (
+    <Image
+      w='184'
+      h='46'
+      m='0 auto'
+      src='assets/images/logo.jpg'
+      alt='Logo worldtrip'
+    />
+  )
+}
