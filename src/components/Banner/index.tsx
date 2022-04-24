005645ff47c671ed2f117ce2b60746a756c805fe
Victor Carli <victorcarlibr@gmail.com>
2022-04-24 01:13:55 -0300

    feat: added banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,74 @@
+import { Box, Image, Text, useBreakpointValue } from '@chakra-ui/react'
+
+export function Banner() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    xl: true
+  })
+
+  return (
+    <Box
+      w='100%'
+      h='335'
+      position='relative'
+    >
+      <Image
+        w='100%'
+        h={[163, 335]}
+        src='assets/images/Banner.jpg'
+        alt='céu com estrelas'
+        objectFit='cover'
+        position='absolute'
+      />
+
+      <Box
+        pr={[5, 10, 120]}
+        pl={[5, 10, 120]}
+      >
+        <Text
+          pt={[15, 20, 20]}
+          position='absolute'
+          fontWeight='bold'
+          color='gray.200'
+          fontSize={['2xl', '4xl', '6xl']}
+        >
+          5 Continentes,
+        </Text>
+
+        <Text
+          top={[12, 32, 36]}
+          position='absolute'
+          fontWeight='bold'
+          color='gray.200'
+          fontSize={['2xl', '4xl', '6xl']}
+        >
+          infinitas possibilidades.
+        </Text>
+
+        <Text
+          w='90%'
+          top={[24, 48, 60]}
+          position='absolute'
+          color='gray.200'
+          fontSize={[14, 20]}
+        >
+          Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+        </Text>
+
+        { isWideVersion && (
+          <Image
+            w='420'
+            h='270'
+            pr={10}
+            src='assets/images/Airplane.png'
+            alt='avião com núvens'
+            objectFit='cover'
+            position='absolute'
+            bottom='-30'
+            right='15'
+          />
+        )}
+      </Box>
+    </Box>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from 'next'
 
 import { Header } from '../components/Header'
+import { Banner } from '../components/Banner'
 
 const Home: NextPage = () => {
-  return <Header />
+  return (
+    <>
+      <Header />
+
+      <Banner />
+    </>
+  )
 }
 
 export default Home
